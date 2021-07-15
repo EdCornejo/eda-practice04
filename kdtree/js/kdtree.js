8c5e87d9e5542808d45662bee368267522c111b3
Ed Cornejo <zed.hyde@icloud.com>
2021-07-14 22:41:23 -0500

    Checkpoint

--- kdtree/js/kdtree.js
+++ kdtree/js/kdtree.js
@@ -12,7 +12,7 @@ class Node {
 function distanceSquared(point1, point2 ){
     var distance = 0;
     for (var i = 0; i < k; i ++)
-        distance += Math.pow ((point1[i] - point2[i]) , 2) ;
+        distance += Math.pow ((point1 [i] - point2 [i]) , 2) ;
     return Math.sqrt ( distance );
 }
 
@@ -29,8 +29,10 @@ function closest_point_brute_force(points, point){
             best_point = points[i];
         }
     }
+    console.log('best_distance', best_distance)
     return best_point;
 }
+
 function naive_closest_point(node, point, depth = 0, best = null){
     //algorithm
     //1. best = min(distance(point, node.point), best)
@@ -52,6 +54,50 @@ function naive_closest_point(node, point, depth = 0, best = null){
     return  naive_closest_point(next_branch, point, depth +1, next_best);
 }
 
+function closest_point(node, point, count, depth = 0, results) {
+    if (node === null)
+        return;
+    var axis = depth % k;
+    var distance = distanceSquared(node.point, point)
+    var i = results.length
+    if (i == 0) {
+        results.push({
+            'node': node,
+            'distance': distance,
+        })
+    }
+    for (i = 0; i < results.length; i++) {
+		if (distance < results[i].distance)
+			break;
+	}
+    // splice in our result
+    if ((i >= 0) &&  (i <= count))
+    {
+        // console.log('splicing in ' + node.point + ' with dist=' + distance + ' at ' + i);
+        results.splice(i, 0, {
+            'node': node,
+            'distance': distance,
+        });
+    }
+    // get rid of any extra results
+	while (results.length > count)
+        results.pop();
+    // whats got the got best _search result? left or right?
+    var goLeft = node.point[axis] < point[axis];
+
+    var target = goLeft ? node.left : node.right;
+    var opposite = goLeft ? node.right : node.left;
+
+    // target has our most likely nearest point, we go down that side of the
+    // tree first
+    if (target)
+        this.closest_point(target, point, count, depth + 1, results);
+
+    // _search the opposite direction, only if there is potentially a better
+    // value than the longest distance we already have in our _search results
+    if ((opposite) && (distanceSquared(opposite.point, point) <= results[results.length - 1].distance))
+        this.closest_point(opposite, point, count, depth + 1, results);
+}
 
 function getHeight(node) {
     if (node === null){
@@ -62,6 +108,7 @@ function getHeight(node) {
     var rh = getHeight(node.right);
     return 1 + max(lh,rh);
 }
+
 function generate_dot(node){
     if (node === null){
         return "";
@@ -104,46 +151,36 @@ function build_kdtree(points, depth = 0){
     return node;
 }
 
-function closer_distance(pivot, point1, point2){
-    if(point1 == null)
-        return point2;
-
-    if(point2 == null)
-        return point1;
-
-    let distance1 = distanceSquared(pivot, point1);
-    let distance2 = distanceSquared(pivot, point2);
-
-    if(distance1 < distance2){
-        return point1;
-    } else {
-        return point2;
-    }
+function contains(rect, point) {
+    var point_inf = rect[0] // inferior izquierdo del rectangulo
+    var point_sup = rect[1] // superior derecho del rectangulo
+    return (
+        point[0]>=point_inf[0] &&
+        point[0]<=point_sup[0] &&
+        point[1]>=point_inf[1] &&
+        point[1]<=point_sup[1]);
 }
 
-function closest_point(root , point , depth = 0){
-    if(root == null)
-        return null;
-
-    let axis = depth % k;
-
-    let next_branch = null;
-    let opposite_branch = null;
-
-    if(point[axis] < root.point[axis]){
-        next_branch = root.left;
-        opposite_branch = root.right;
-    } else {
-        next_branch = root.right;
-        opposite_branch = root.left;
+function range_query_rect(node , rect , found , depth = 0) {
+    if (node === null) {
+        return;
     }
-
-    let best = closer_distance(point, closest_point(next_branch, point, depth + 1), root.point)
-
-    if(distanceSquared(point, best) > Math.abs(point[axis] - root.point[axis])){
-        best = closer_distance(point, closest_point(opposite_branch, point, depth + 1), best);
+    var axis = depth % k;
+    if (node.point[axis] < rect[axis][0]){
+        range_query_rect(node.right, rect, found, depth+1)
+        return
     }
-    return best;
+    if (node.point[axis] > rect[axis][1]) {
+        range_query_rect(node.left, rect, found, depth+1)
+        return
+    }
+    let x = node.point[0]
+    let y = node.point[1]
+    if (!(rect[0][0]>x || rect[0][1]<x || rect[1][0]>y || rect[1][1]<y)) { // test node in rect
+        found.push(node.point)
+    }
+    range_query_rect(node.left, rect, found, depth+1)
+    range_query_rect(node.right, rect, found, depth+1)
 }
 
 function kdCompare(root, point, depth){
@@ -155,72 +192,79 @@ function kdCompare(root, point, depth){
     }
 }
 
-function range_query_rect(root, rect, queue, depth=0){
-
-}
-
-
 function range_query_circle(root , center , radio , queue , depth = 0) {
-
-    if(root == null){
+    if(root == null) {
         return null;
     }
-    console.log(root, [center[0], center[1]], radio);
-    console.log(kdCompare(root, [center[0]-radio, center[1]-radio], depth));
+    // console.log(root, [center[0], center[1]], radio);
+    // console.log(kdCompare(root, [center[0]-radio, center[1]-radio], depth));
     if(kdCompare(root, [center[0]-radio, center[1]-radio], depth) > 0){
         range_query_circle(root.right, center, radio, queue, depth + 1)
         return null;
     }
-
     if(kdCompare(root, [center[0]+radio, center[1]+radio], depth) < 0){
         range_query_circle(root.left, center, radio, queue, depth + 1)
         return null;
     }
-
     if(distanceSquared(center, root.point) <= radio){
         queue.push(root.point)
     }
-
     range_query_circle(root.left, center, radio, queue, depth + 1)
     range_query_circle(root.right, center, radio, queue, depth + 1)
     return null;
-
 }
 
-// digraph G {
-//     "106 ,189 " -> "6 ,114";
-//     "6 ,114" -> " 90 ,102";
-//     "90 ,102 " -> "21 ,84";
-//     "6 ,114" -> " 84 ,138";
-//     "84 ,138 " -> "5 ,150";
-//     "106 ,189 " -> "148 ,85 ";
-//     "148 ,85 " -> "181 ,45 ";
-//     "181 ,45 " -> "161 ,29 ";
-//     "148 ,85 " -> "158 ,120 ";
-// }
 
+let INF = 9999999999999;
 
+function update_neighbors(p0, p, neighbors, n) {
+    d = distanceSquared(p, p0)
 
+    console.log(p, p0)
+    console.log('d', d)
+    // for (i, x in enumerate(neighbors)) {
+    for (let i = 0; i < neighbors.length; i ++) {
+    var x =  neighbors[i]
+        if (i == n) {
+            return neighbors[n-1][1]
+        }
+        if (d < x[1]) {
+            neighbors.splice(i, 0, [p0, d])
+            if (neighbors.length < n) {
+                return INF
+            }
+            return neighbors[n-1][1]
+        }
+    }
+    neighbors.push([p0, d])
+    return INF
+}
 
-// var data = [
-//     [40 ,70] ,
-//     [70 ,130] ,
-//     [90 ,40] ,
-//     [110 , 100] ,
-//     [140 ,110] ,
-//     [160 , 100]
-// ];
+let maxdist
 
-// var point = [140 ,90]; // query
+function nnquery(t, p, n, found, depth=0) {
+    if (t == null) {
+        return null
+    }
+    if (t.left == null & t.right == null) {
+        maxdist = update_neighbors(t.point, p, found, n)
+        return
+    }
+    axis = depth % p.length
+    var nearer_tree
+    var farther_tree
+    if (p[axis] < t.point[axis]) {
+        nearer_tree = t.left
+        farther_tree = t.right
+    } else {
+        nearer_tree = t.right
+        farther_tree = t.left
 
+    }
+    nnquery(nearer_tree, p, n, found, depth+1)
+    maxdist = update_neighbors(t.point, p, found, n)
+    if (Math.abs(t.point[axis]-p[axis]) < maxdist) // must check the far side
+        nnquery(farther_tree, p, n, found, depth+1)
+    return
+}
 
-// var data = [
-//     [40 ,70] ,
-//     [70 ,130] ,
-//     [90 ,40] ,
-//     [110 , 100] ,
-//     [140 ,110] ,
-//     [160 , 100] ,
-//     [150 , 30]
-// ];
-// var point = [140 ,90]; // query
